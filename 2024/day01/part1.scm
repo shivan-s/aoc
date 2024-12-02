6c8ba45da82243d4668be061a650a09164fc2323
Shivan Sivakumaran <me@shivan.xyz>
2024-12-01 22:05:49 -0800

    chore: day 1 wip and incomplete

--- 2024/day01/part1.scm
+++ 2024/day01/part1.scm
@@ -1,3 +1,12 @@
 #lang scheme
 
-(+ 1 1)
+(define (read-input file)
+	(read-string 1000 (open-input-file file))
+)
+
+(map (string-split) (string-split (read-input "2024/day01/sample.txt") "\n"))
+; obtain list of numbers
+; obtains lists as n and 2n index
+; sort these lists
+; iterate over lists getting the difference between the element
+; sum these diffs
